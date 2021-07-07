45789f147c6a89e255e61657dac25e0e6ece1893
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-07 19:18:28 +0100

    Comment Your JavaScript Code

--- file.js
+++ file.js
@@ -1,1 +1,12 @@
-console.log ("Hello World");
+// this is a line of code
+
+// singular lines of code 
+
+// good for in line commentry
+
+/* This is a multi line
+comment. You can use this to 
+comment out many lines
+so that Javascript ignores
+the content written 
+within */
