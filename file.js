4eab74d499543af731c43a71048e688f08944279
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-14 21:35:39 +0100

    Understanding Uninitialized Variables

--- file.js
+++ file.js
@@ -38,14 +38,27 @@ var a;
 a = 7;
 var b;
 
+b = a;
+console.log(b);
+
 // Initializing Variables with the Assignment Operator
 
 var a = 9
 console.log(a);
 
+// Understanding Uninitialized Variables
 
-b = a;
-console.log(b);
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = " I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+console.log(c)
 
 
 
