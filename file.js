0a7ffe3f28938a90b6a33a73228005dcf1996f99
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-18 11:08:39 +0100

    Compound Assignment With Augmented Division

--- file.js
+++ file.js
@@ -181,6 +181,21 @@ b *= 3;
 c *= 10;
 console.log(a,b,c);
 
+// Compound Assignment With Augmented Division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a, b, c)
+
+
+
 
 
 
