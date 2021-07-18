803df2a3e88d9d3065de625f4477df155afa2b91
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-18 22:21:27 +0100

    Escaping Literal Quotes in Strings

--- file.js
+++ file.js
@@ -209,6 +209,12 @@ var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Chan
 
 console.log(myStr)
 
+// Quoting Strings with Single Quotes
+
+// var myStr = '<a href=\"http://www.example.com\" target=\"_blank\">Link</a>';
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr)
 
 
 
