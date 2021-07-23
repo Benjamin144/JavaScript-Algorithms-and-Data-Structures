a3adeda5b53d16f13a582ec0f559cfe6faf77246
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-23 23:34:27 +0100

    Access Multi-Dimensional Arrays With Indexes

--- file.js
+++ file.js
@@ -397,6 +397,29 @@ var joesArray = [1,44,144000];
 joesArray[2] = [29];
 console.log(joesArray);
 
+// Access Multi-Dimensional Arrays With Indexes
+
+var arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [[10,11,12], 13, 14]
+  ];
+  arr[3];
+  arr[3][0];
+  arr[3][0][1];
+
+  // arr[3] is [[10, 11, 12], 13, 14], arr[3][0] is [10, 11, 12], and arr[3][0][1] is 11.
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
+
+console.log (myData);
+
+
+
+
 
 
 
