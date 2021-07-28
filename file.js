c0f8e789b3d3ecf4208b63f18477df7d75c62f71
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-07-28 16:51:15 +0100

    Global Scope and Functions

--- file.js
+++ file.js
@@ -528,7 +528,28 @@ function functionWithArgs(x, y) {
   functionWithArgs(1, 2);
   functionWithArgs(6, 2);
 
+  // Global Scope and Functions
 
+// Declare the myGlobal variable below this line
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal = 5;
+}
+ 
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
 
 
