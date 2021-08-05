360bcc979e947755a06c17c1562b8aa0de0ad490
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-08-05 22:36:46 +0100

    Comparison with the Greater Than Or Equal To Operator

--- file.js
+++ file.js
@@ -829,8 +829,6 @@ function testStrictNotEqual(val) {
 
 testStrictNotEqual(10);
 
-*/
-
 // Comparison with the Greater Than Operator
 
 function testGreaterThan(val) {
@@ -846,6 +844,24 @@ function testGreaterThan(val) {
 }
 
 testGreaterThan(10);
+
+*/
+
+// 
+
+function testGreaterOrEqual(val) {
+  if (val >='20') {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
 
 
 
