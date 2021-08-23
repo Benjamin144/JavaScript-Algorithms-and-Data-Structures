be275be00afcc31c27b76243d2690d56300dff4d
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2021-08-23 23:03:49 +0100

    Selecting from many options with switch statements

--- file.js
+++ file.js
@@ -1032,8 +1032,6 @@ function testSize(num) {
 
 testSize(7);
 
-*/
-
 // Golf Code
 
 var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
@@ -1061,7 +1059,37 @@ function golfScore(par, strokes) {
   }
 
   // Only change code above this line
+
+  // Selecting from Many Options with Switch Statements
 }
 
 golfScore(5, 4);
 
+*/
+
+// Selecting from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
