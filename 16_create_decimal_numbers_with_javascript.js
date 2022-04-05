12e914863c1bb4460426ab77e0e53d32284c674f
Benjamin144 <josephroberts@email.com>
2022-04-05 19:11:29 +0000

    new file added about fpn's

---
+++ 16_create_decimal_numbers_with_javascript.js
@@ -0,0 +1,7 @@
+// Decimal numbers are sometimes referred to as floating point numbers or floats.
+
+const ourDecimal = 5.7;
+var myDecimal = 5.7; 
+console.log(ourDecimal, myDecimal)
+
+// Not all real numbers can accurately be represented in floating point. This can lead to rounding errors
