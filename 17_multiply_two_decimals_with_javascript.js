12e875bfebed02ab18a2bdbf8eaaa0cebf7abd9d
Benjamin144 <josephroberts@email.com>
2022-04-05 19:33:36 +0000

    added new file rev: dividing quotients

---
+++ 17_multiply_two_decimals_with_javascript.js
@@ -0,0 +1,6 @@
+// you can also perform calculations with decimal numbers, just like whole numbers.
+
+const product = 2.0 * 0.0;
+let products = 2.0 * 2.5;
+
+console.log (product, products)
---
+++ 18_divide_one_decimal_by_another_with_javascript.js
@@ -0,0 +1,5 @@
+// dividing one decimal by another
+
+const quotient = 0.0 / 2.0;
+let quotients = 4.4 / 2.0;
+console.log(quotient, quotients)
