3caff863f0da48e62683aef91eda7a18672d1712
Benjamin144 <josephroberts@email.com>
2022-04-06 20:13:13 +0000

    compound_assignment_with_augmented_subtraction

---
+++ 21_compound_assignment_with_augmented_subtraction.js
@@ -0,0 +1,20 @@
+/*
+Like the += operator, -= subtracts a number from a variable.
+myVar = myVar - 5;
+will subtract 5 from myVar. This can be rewritten as:
+
+myVar -= 5; */
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// this converts the assignment for a,b & c to usw -= operator by shorthand 
+
+a -= 6; 
+b -= 15; 
+c -= 1; 
+
+console.log(a,b,c)
+
+
