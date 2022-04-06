e3e7875bce206609add0814241f643fc8ae00906
Benjamin144 <josephroberts@email.com>
2022-04-06 20:32:24 +0000

    compound_assignment_with_augmented_multiplication

---
+++ 22.compound_assignment_with_augmented_multiplication.js
@@ -0,0 +1,18 @@
+// 21_compound_assignment_with_augmented_subtraction.js
+
+/* myVar = myVar * 5;
+will multiply myVar by 5. This can be rewritten as:
+
+myVar *= 5; */
+
+// This code converts the assignments for a, b, and c to use the *= operator.
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a,b,c)
