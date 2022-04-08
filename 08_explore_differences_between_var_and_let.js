bae87e1b8e46b6326788e311b75d6e529b40dc42
Benjamin144 <josephroberts@email.com>
2022-04-08 20:20:35 +0000

    declaring blocked-scoped local variables

--- 08_explore_differences_between_var_and_let.js
+++ 08_explore_differences_between_var_and_let.js
@@ -16,4 +16,20 @@ let catSound = "Meow!";
 console.log(catName)
 console.log(catSound)
 
+// The let statement declares a block-scoped local variable, optionally initializing it to a value.
+
+let x = 1;
+
+if (x === 1) {
+  let x = 2;
+
+  console.log(x);
+  // expected output: 2
+}
+
+console.log(x);
+// expected output: 1
+
+
+
 
