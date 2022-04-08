3f579c42fb287cf7aeeea69ea4c7a72e15aa5772
Benjamin144 <josephroberts@email.com>
2022-04-08 22:13:24 +0000

    update compound assignment with augmented division

---
+++ 23.compound_assignment_with_augmented_division.js
@@ -0,0 +1,17 @@
+/* Compound Assignment With Augmented Division
+The /= operator divides a variable by another number.
+
+myVar = myVar / 5;
+Will divide myVar by 5. This can be rewritten as:
+
+myVar /= 5; */
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12; [4] // denoting the answer to this value function
+b /= 4; [27]
+c /= 11; [3]
+
+console.log (a,b,c)
---
+++ 24.escaping_literal_quotes_in_strings.js
@@ -0,0 +1,6 @@
+/* I am using a backslashes to assign a string to the myStr variable 
+so that if you were to print it to the console, you would see:
+I am a "double quoted" string inside "double quotes". */
+
+const myStr ="I am a \"double quoted\" string inside \"double quotes\".";
+console.log(myStr)
