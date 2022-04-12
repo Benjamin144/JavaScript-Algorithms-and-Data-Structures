ce4338020eaae0ca194c86e947d61eaf677bd2f1
Benjamin144 <josephroberts@email.com>
2022-04-12 17:36:58 +0000

    added & comp ex 28.concatenating_strings_with_+-_operators.js

---
+++ 28.concatenating_strings_with_+-_operators.js
@@ -0,0 +1,9 @@
+/* let ourStr = "I come first. ";
+ourStr += "I come second."; // adding more string content to assigned variable
+
+console.log(ourStr) */
+
+let myStr = "This is the first sentance. ";
+myStr += "This is the second sentance. ";
+myStr += "This is the third sentance.";
+console.log(myStr)
