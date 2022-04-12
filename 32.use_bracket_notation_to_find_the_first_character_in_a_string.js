dfd47ffc8cf6fa0136f1d16266adae5460d1db05
Benjamin144 <josephroberts@email.com>
2022-04-12 20:19:32 +0000

    added/comp ex 32.use_bracket_notation_to_find_the_first_character_in_a_string.js

---
+++ 32.use_bracket_notation_to_find_the_first_character_in_a_string.js
@@ -0,0 +1,12 @@
+/* 32.use_bracket_notation_to_find_the_first_character_in_a_string.js
+
+const firstName = "Joseph";
+const fourthLetter = firstName[4];
+
+console.log(fourthLetter) */
+
+let lastName = "MaryJBlige";
+const firstLetterOfLastName = lastName[4];
+console.log(firstLetterOfLastName)
+
+
