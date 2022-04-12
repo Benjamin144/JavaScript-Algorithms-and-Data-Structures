afcc0ac709e073aa0492d3688c4d8aae38995f6e
Benjamin144 <josephroberts@email.com>
2022-04-12 19:40:24 +0000

    added/comp ex 31.find_the_length_of_a_string.js

---
+++ 31.find_the_length_of_a_string.js
@@ -0,0 +1,12 @@
+/* 31.find_the_length_of_a_string.js
+
+myName = "Joseph"
+console.log(myName.length) */
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+lastNameLength = lastName.length
+console.log(lastNameLength)
+
+/* Used the .length property to count the number of characters in the lastName variable and assign it to lastNameLength. */
