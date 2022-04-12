33551625f2fd1b8551fd884fa159ffc6da74de30
Benjamin144 <josephroberts@email.com>
2022-04-12 18:35:05 +0000

    added/comp ex 29.constructing_strings_with_variables.js

---
+++ 29.constructing_strings_with_variables.js
@@ -0,0 +1,9 @@
+/* const ourName = "freeCodeamp";
+const ourStr = "Hello our name is " + ourName + ", how are you?";
+
+console.log(ourStr) */
+
+const myName = "Joseph";
+const myStr = "My name is " + myName + ", and I am well!";
+
+console.log(myStr)
