d0a5e3bdb4cf1a0f4090c9978c4b9d4fa7112c80
Benjamin144 <josephroberts@email.com>
2022-04-14 21:05:25 +0000

    35.use_bracket_notation_to_find_the_last_character_in_a_string.js

---
+++ 35.use_bracket_notation_to_find_the_last_character_in_a_string.js
@@ -0,0 +1,11 @@
+/* 35.use_bracket_notation_to_find_the_last_character_in_a_string.js
+
+const firstName = "Ada";
+lastLetter = firstName[firstName.length -3]; -3,-2,-1 Try it yourself!
+
+console.log(lastLetter) */
+
+
+const lastName = "Lovelace";
+const lastLetterOfLastName = lastName[lastName.length -1]; 
+console.log(lastLetterOfLastName)
