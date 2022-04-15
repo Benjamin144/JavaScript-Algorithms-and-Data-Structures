445944b4ddbae097cbe6544cb59285cc70bb9239
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2022-04-15 20:38:29 +0100

    added/comp ex 36.word_blank.js

---
+++ 36.word_blank.js
@@ -0,0 +1,8 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+let wordBlanks = "I used to have a lovely " + myNoun + " the mutter was very " + myAdjective + ". ";
+wordBlanks += "It went and " + myVerb + " " + myAdverb + " around the block and scared the living day lights out of my neighours.";
+console.log(wordBlanks);
