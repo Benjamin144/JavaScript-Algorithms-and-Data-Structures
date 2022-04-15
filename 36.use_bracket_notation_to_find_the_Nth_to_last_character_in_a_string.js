b1db412d3d7809e2398d8e6f496324c69a9c8fc8
Benjamin144 <josephroberts@email.com>
2022-04-15 15:46:25 +0000

    36.use_bracket_notation_to_find_the_Nth_to_last_character_in_a_string.js

---
+++ 36.use_bracket_notation_to_find_the_Nth_to_last_character_in_a_string.js
@@ -0,0 +1,6 @@
+const firstName = "Joseph";
+const thirdToLastLetter = firstName[firstName.length -3];
+
+const lastName = "Roberts";
+const secondToLastLetter = lastName[lastName.length -6]; // For example, you can get the value of the sixth-to-last letter of the const firstName = "Roberts" string by using firstName[firstName.length - 6]
+console.log(secondToLastLetter)
