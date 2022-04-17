109e8bd54414a8cbc94d9788bdd5a394a7a1471f
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2022-04-17 21:24:17 +0100

    added/comp ex 38.store_multiple_values_in_one_variable_using_javascript.js

---
+++ 38.store_multiple_values_in_one_variable_using_javascript.js
@@ -0,0 +1,14 @@
+// // With JavaScript array variables, we can store several pieces of data in one place.
+
+const sandwich = ["peanut butter", "jelly", "bread"];
+
+console.log(sandwich)
+
+const myArray = ["peanut butter", 1, "jelly", 2, "bread", 3]
+
+console.log(myArray)
+
+// Only change code below this line
+var myArrays = ["Children",1,"of",2,"Israel",3];
+
+console.log(myArrays)
