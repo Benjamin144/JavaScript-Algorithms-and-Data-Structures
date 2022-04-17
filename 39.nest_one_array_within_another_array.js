4f2d8a1c4b48833a835d48bca626dd638bdbdd55
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2022-04-17 21:59:18 +0100

    created a variable called myData and set it to equal the first value of myArray using bracket notation

---
+++ 39.nest_one_array_within_another_array.js
@@ -0,0 +1,7 @@
+// You can also nest arrays within other arrays, like below:
+const teams = [["Bulls", 1], ["White Sox", 45]];
+
+console.log(teams)
+
+const myArray = [["Joseph",31],["Anthony",1],["Roberts",1971]];
+console.log(myArray)
