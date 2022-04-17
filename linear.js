fb3214099b38e1fb2dd13ffdb9c89220aa6b21b8
Benjamin144 <59931152+Benjamin144@users.noreply.github.com>
2022-04-17 17:48:24 +0100

    added example code for Big O Linear expression

---
+++ linear.js
@@ -0,0 +1,12 @@
+// Big O(n) Function
+
+function linearFunc(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(1000 * 10000)
+    }
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7]
+linearFunc(arr)
+
+// an example of how say how long a dentist would take to treat x amount of patients
