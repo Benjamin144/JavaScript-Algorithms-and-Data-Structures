4de86657c03ad3aa35e30b3755e68fb4518eafc6
Benjamin144 <josephroberts@email.com>
2022-04-25 21:06:10 +0000

    added modify_array_data_with_indexes.js

---
+++ modify_array_data_with_indexes.js
@@ -0,0 +1,12 @@
+// modify_array_data_with_indexes.js  
+
+
+const ourArray = [50, 40, 30];
+ourArray[0] = [15]; // modifying this arry makes dealing with arrays mutable despite using a 'const' variable
+console.log(ourArray)
+
+// example two arrays
+const myArray = [18, 64, 99]
+myArray[0] = [45]
+
+console.log(myArray);
