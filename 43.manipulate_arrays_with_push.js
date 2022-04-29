6026a011f769ce704537a2386051b0ea71bf2c1c
Joseph Roberts <jojumbotron@gmail.com>
2022-04-29 17:48:36 +0100

    added/comp ex manipulate arrays with pop()

---
+++ 43.manipulate_arrays_with_push.js
@@ -0,0 +1,14 @@
+/* An easy way to append data to the end of an array is via the push() function.
+.push() takes one or more parameters and "pushes" them onto the end of the array. */
+
+const arr1 = [1, 2, 3];
+arr1.push(4)
+
+const arr2 = [["Roberts", "J", "Milo"]]
+arr2.push(["happy", "joy"]);
+console.log(arr2)
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+
+console.log(myArray)
