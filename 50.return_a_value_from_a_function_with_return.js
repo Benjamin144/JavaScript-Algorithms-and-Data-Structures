807993f8f4f171199f961347d602766abff4e319
Benjamin144 <josephroberts@email.com>
2022-06-03 15:46:16 +0000

    50.return_a_value_from_a_function_with_return

---
+++ 50.return_a_value_from_a_function_with_return.js
@@ -0,0 +1,26 @@
+// We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
+function plusThree(num) { 
+    return num + 3
+}
+const answer  = plusThree(5)
+
+// plusThree takes an argument for num and returns a value equal to num + 3.
+
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesfive(num) {
+    return num * 5;
+}
+
+const answer2 = timesfive(144);
+console.log(answer2)
+
+// example 2
+
+function timesFive(z){
+    return z * 5;
+  }
+  
+  const myAnswer = timesFive(888);
+  console.log(myAnswer);
