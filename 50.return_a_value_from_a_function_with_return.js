e0df161f70b264a5b2713de72f97403e24a414b1
Benjamin144 <josephroberts@email.com>
2022-06-14 19:03:49 +0000

    solved global scope challenge

--- 50.return_a_value_from_a_function_with_return.js
+++ 50.return_a_value_from_a_function_with_return.js
@@ -1,17 +1,28 @@
-// We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+/* We can pass values into a function with arguments. You can use a return statement to send a value back out of a function.
+
 
 function plusThree(num) { 
     return num + 3
 }
 const answer  = plusThree(5)
 
+console.log(answer)
+
 // plusThree takes an argument for num and returns a value equal to num + 3.
 
 // Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
 
-function timesfive(num) {
-    return num * 5;
+*/
+
+function time(zones) {
+    return zones != 4
 }
+
+const period = time(4)
+console.log(period)
+
+
+/*
 
 const answer2 = timesfive(144);
 console.log(answer2)
@@ -24,3 +35,4 @@ function timesFive(z){
   
   const myAnswer = timesFive(888);
   console.log(myAnswer);
+*/
