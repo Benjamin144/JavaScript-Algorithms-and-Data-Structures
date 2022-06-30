08a9f1acd5e0a684c976680c99313ce011ad7751
Benjamin144 <josephroberts@email.com>
2022-06-30 19:20:22 +0000

    host.json file added to attempt to run function

--- name_manipulation._v2.js
+++ name_manipulation._v2.js
@@ -1,7 +1,16 @@
-function {
-    var x = prompt('What is your name?');
-    var y = x.slice(0,1);
-    var z = x.slice(1, x.length);
-    alert('Hello ' + y.toUpperCase() + z.toLocaleLowerCase() + ' ' + surname + ' Your age is ' + age + '. You live in ' + address); 
-
+function input() {
+  var x = prompt("What is your name?");
+  var y = x.slice(0, 1);
+  var z = x.slice(1, x.length);
+  alert(
+    "Hello " +
+      y.toUpperCase() +
+      z.toLowerCase() +
+      " " +
+      surname +
+      " Your age is " +
+      age +
+      ". You live in " +
+      address
+  );
 }
