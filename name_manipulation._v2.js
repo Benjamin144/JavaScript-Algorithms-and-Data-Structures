b38d80a27cc18e20b8054d10436a8c606664acd9
Benjamin144 <josephroberts@email.com>
2022-07-03 18:55:53 +0000

    revised nme manipulation because of problem with variale naming conventon

--- name_manipulation._v2.js
+++ name_manipulation._v2.js
@@ -1,16 +1,28 @@
-function input(age, address, surname) {
-  var x = prompt("What is your name?");
-  var y = x.slice(0, 1);
-  var z = x.slice(1, x.length);
+// function input(age, address, surname) {
+//   var x = prompt("What is your name?");
+//   var y = x.slice(0, 1);
+//   var z = x.slice(1, x.length);
+//   alert(
+//     "Hello " +
+//       y.toUpperCase() +
+//       z.toLowerCase() +
+//       " " +
+//       surname +
+//       " Your age is " +
+//       age +
+//       ". You live in " +
+//       address
+//   );
+// }
+
+function greeting(age, address, surname) {
+  var input = prompt("Hello, what is your name?");
+  var firstCapsID = input.slice(0, 1);
+  var restOfLettersID = input.slice(1, input.length);
+  var finalName =
+    firstCapsID.toUpperCase() + restOfLettersID.toLowerCase() + " " + surname;
   alert(
-    "Hello " +
-      y.toUpperCase() +
-      z.toLowerCase() +
-      " " +
-      surname +
-      " Your age is " +
-      age +
-      ". You live in " +
-      address
+    "Hello " + finalName + " Your age is " + age + " and you live in " + address
   );
+  return finalName;
 }
